9bde5e12540096e3eee2064d4516d64f1626c64d
SAIKIRAN-DASARI <110319300+SAIKIRAN-DASARI@users.noreply.github.com>
2023-10-18 00:41:37 +0530

    Delete spring directory

--- spring/src/main/java/com/example/spring1/spring/Respository/IAddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring1.spring.Respository;
-
-import com.example.spring1.spring.Models.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAddressRepo extends JpaRepository<Address,String> {
-}
